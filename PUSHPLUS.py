8d4eeced39df719bed1277ed4e7f1a02aeecb833
空门苍天 <80729244+gyx5019@users.noreply.github.com>
2024-10-19 15:47:36 +0800

    Update PUSHPLUS.py

--- PUSHPLUS.py
+++ PUSHPLUS.py
@@ -1,6 +1,6 @@
 import requests
 import os
-token = os.environ['TOKEN']
+token = os.environ['9fff214b3d7f4b879ce08477e788af5d']
 title= 'Epic-FreeGamer'
 content ='Epic-FreeGamer任务已执行'
 url = 'http://www.pushplus.plus/send?token='+token+'&title='+title+'&content='+content
